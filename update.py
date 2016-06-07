dbd54b40cbea318ad03fa76149393f2725a823bf
phntxx <meissner.bastian@gmail.com>
2016-06-07 21:33:29 +0200

    Update update.py

--- update.py
+++ update.py
@@ -47,7 +47,6 @@ with open('/var/www/html/api.json', 'r') as f:
     json_data['ram_free'] = RAM_free
     json_data['ram_used'] = RAM_used
     json_data['space_free'] = DISK_free
-    json_data['space_used'] = DISK_used
     json_data['cpuusage'] = CPU_usage
 
 with open('/var/www/html/api.json', 'w') as f:
