79a3cc0ff4e767c4a0d1bce4440ec999df91044c
phntxx <meissner.bastian@gmail.com>
2016-06-26 18:42:57 +0200

    Update update.py

--- update.py
+++ update.py
@@ -53,8 +53,8 @@ with open('/var/www/html/api.json', 'r') as f:
     json_data['temperature'] = CPU_temp
     json_data['ram_free'] = RAM_free
     json_data['ram_used'] = RAM_used
-    json_data['space_free'] = DISK_free
-    json_data['space_used'] = DISK_used
+    json_data['ram_free'] = str(RAM_free) + "MB"
+    json_data['ram_used'] = str(RAM_used) + "MB"
     json_data['cpuusage'] = CPU_usage
 
 with open('/var/www/html/api.json', 'w') as f:
