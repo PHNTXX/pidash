5e8807b87d6f80b5696d34de5eb759a9c488a2d3
phntxx <meissner.bastian@gmail.com>
2016-11-08 17:59:20 +0100

    Update update64.py

--- update64.py
+++ update64.py
@@ -42,20 +42,7 @@ DISK_free = DISK_stats[2]
 DISK_perc = DISK_stats[3]
 
 print(CPU_temp)
-print(CPU_usage)
 print(RAM_free)
 print(RAM_used)
 print(DISK_free)
 print(DISK_used)
-
-with open('/var/www/html/api.json', 'r') as f:
-    json_data = json.load(f)
-    json_data['temperature'] = CPU_temp
-    json_data['ram_free'] = str(RAM_free) + "MB"
-    json_data['ram_used'] = str(RAM_used) + "MB"
-    json_data['space_free'] = DISK_free
-    json_data['space_used'] = DISK_used
-    json_data['cpuusage'] = CPU_usage
-
-with open('/var/www/html/api.json', 'w') as f:
-    f.write(json.dumps(json_data))
