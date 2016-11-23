b4fac6dd7aa646caa066e5d9823a7d6b5fd2267d
phntxx <meissner.bastian@gmail.com>
2016-11-23 20:44:03 +0100

    Update api.php

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 
-$input = shell_exec('python /var/www/html/pidash/update.py');
+$input = shell_exec('python update.py');
 $array = explode("\n", $input);
 $output = array('temperature' => $array[0], 'ram_free' => $array[1], 'ram_used' => $array[2], 'space_free' => $array[3], 'space_used' => $array[4]);
 echo json_encode($output);
