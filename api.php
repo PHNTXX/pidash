046e3ee5a94d10ff81dc5e0ed4f133eebb0574db
phntxx <meissner.bastian@gmail.com>
2016-12-02 19:01:44 +0100

    Update api.php

--- api.php
+++ api.php
@@ -5,6 +5,8 @@ if($_GET["passwd"] == "none") {
   $input = shell_exec('python update.py');
   $array = explode("\n", $input);
   $output = array('temperature' => $array[0], 'ram_free' => $array[1], 'ram_used' => $array[2], 'space_free' => $array[3], 'space_used' => $array[4]);
-  echo json_encode($output);
+} else {
+  $output = array('ram_free' => 'password invalid.');
 }
+  echo json_encode($output);
 ?>
