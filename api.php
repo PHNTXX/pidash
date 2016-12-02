04f109cb6a529116875215ba30c3e633fdbf2bea
phntxx <meissner.bastian@gmail.com>
2016-12-02 18:58:30 +0100

    Update api.php

--- api.php
+++ api.php
@@ -1,9 +1,10 @@
 <?php
 header('Content-Type: application/json');
 
-$input = shell_exec('python update.py');
-$array = explode("\n", $input);
-$output = array('temperature' => $array[0], 'ram_free' => $array[1], 'ram_used' => $array[2], 'space_free' => $array[3], 'space_used' => $array[4]);
-echo json_encode($output);
-
+if($_GET["passwd"] == "none") {
+  $input = shell_exec('python update.py');
+  $array = explode("\n", $input);
+  $output = array('temperature' => $array[0], 'ram_free' => $array[1], 'ram_used' => $array[2], 'space_free' => $array[3], 'space_used' => $array[4]);
+  echo json_encode($output);
+}
 ?>
