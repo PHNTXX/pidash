3131a9eb3f830bb74a5751aad9db7ac62483fcf3
Bastian Meissner <phntxx@gmail.com>
2019-03-29 21:32:19 +0100

    Rewrote api.php
    sbcDash now only requires api.php to function

--- api.php
+++ api.php
@@ -1,12 +1,51 @@
 <?php
 header('Content-Type: application/json');
-if($_GET["passwd"] == "none") { //change "none" to your preferred password, the app uses "none" when there is no custom password
-  $input = shell_exec('python update.py');
-  $array = explode("\n", $input);
-  $ipaddr = $_SERVER['SERVER_ADDR'];
-  $output = array('temperature' => $array[0], 'ram_free' => $array[1], 'ram_used' => $array[2],'ram_perc' => $array[6], 'space_free' => $array[3], 'space_used' => $array[4], 'space_perc' => $array[5], 'webserver' => $ipaddr);
-} else {
-  $output = array('temperature' => "NO", 'ram_free' => "PASSWORD", 'ram_used' => "INVALID", 'space_free' => "PASSWORD", 'space_used' => "INVALID", 'webserver' => "INVALID");
+
+// Uncomment the following line and add a password if you want one.
+// $password =
+
+// Return current CPU temperature
+function getCPUTemperature () {
+  return (exec('cat /sys/class/thermal/thermal_zone0/temp')) / 1000;
 }
-echo json_encode($output);
+
+// Return current RAM usage. [free, used]
+function getRAMUsage () {
+  exec('free', $usage);
+  preg_match_all('/\s+([0-9]+)/', $usage[1], $value);
+  return array($value[1][1], $value[1][2]);
+}
+
+// Return current disk usage. [size, used, available, usage]
+function getDiskUsage () {
+  exec ('df -h /', $usage);
+  preg_match_all('/\s+([0.00-9.00]+([A-Z]|[%]))/', $usage[1], $value);
+  return $value[1];
+}
+
+function returnData () {
+  return json_encode(array(
+    'temperature' => getCPUTemperature(),
+    'ram_free' => getRAMUsage()[0],
+    'ram_used' => getRAMUsage()[1],
+    'ram_perc' => (getRAMUsage()[1] / getRAMUsage()[0]),
+    'disk_free' => getDiskUsage()[2],
+    'disk_used' => getDiskUsage()[1],
+    'disk_perc' => getDiskUsage()[3],
+    'webserver' => $_SERVER['SERVER_ADDR']
+  ));
+}
+
+if (isset($password)) {
+  if ($_GET['password'] == $password) {
+    echo returnData();
+  } else {
+    echo json_encode(array(
+      'error' => '403: invalid password.'
+    ));
+  }
+} else {
+  echo returnData();
+}
+
 ?>
