38f6a6957fb16c474e27eae567308aa61d19358e
Bastian Meissner <meissner.bastian@gmail.com>
2019-04-21 18:37:18 +0200

    Added more data regarding RAM usage

--- api.php
+++ api.php
@@ -9,11 +9,11 @@ function getCPUTemperature () {
   return (exec('cat /sys/class/thermal/thermal_zone0/temp')) / 1000;
 }
 
-// Return current RAM usage. [free, used]
+// Return current RAM usage. [total, used, free, shared, buff/cache, available]
 function getRAMUsage () {
   exec('free', $usage);
   preg_match_all('/\s+([0-9]+)/', $usage[1], $value);
-  return array($value[1][1], $value[1][2]);
+  return array($value[1][0], $value[1][1], $value[1][2], $value[1][3], $value[1][4], $value[1][5]);
 }
 
 // Return current disk usage. [size, used, available, usage]
@@ -26,8 +26,12 @@ function getDiskUsage () {
 function returnData () {
   return json_encode(array(
     'temperature' => getCPUTemperature(),
-    'ram_free' => getRAMUsage()[0],
+    'ram_total' => getRAMUsage()[0],
     'ram_used' => getRAMUsage()[1],
+    'ram_free' => getRAMUsage()[2],
+    'ram_shared' => getRAMUsage()[3],
+    'ram_buff' => getRAMUsage()[4],
+    'ram_available' => getRAMUsage()[5],
     'ram_perc' => (getRAMUsage()[1] / getRAMUsage()[0]),
     'disk_free' => getDiskUsage()[2],
     'disk_used' => getDiskUsage()[1],
